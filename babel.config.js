eceffc4c130575eabb7916da4edf952f0eb9eb55
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-07-03 00:37:31 +0900

    chore: add comment

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+// https://jestjs.io/ja/docs/getting-started#typescript-%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
